892b3bd671656ebc1fadea46d02087e422477a3d
Pujan <130989697+pujangampa@users.noreply.github.com>
2024-01-09 21:51:26 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,11 @@
 #To build the docker image with specific tag
 docker build -t pujangampa/webapp:latest .
 
+#To run the webapp with specific port
 docker run -d -p 5000:5000 pujangampa/webapp:latest
 
+#Tag the local image with Docker Hub username and repo name
 docker tag pujangampa/webapp:latest pujangampa/webapp:latest
 
+#Push the docker image to Docker Hub
 docker push pujangampa?webapp:latest
